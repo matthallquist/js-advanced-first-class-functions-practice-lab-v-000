ba4a404f59caec917a492dc2f1f2236dedd7d902
Matthew Hallquist <matthallquist@yahoo.com>
2018-08-28 20:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const logDriverNames = function(drivers) {
+  drivers.map(driver => console.log(driver.name));
+}
+
+const logDriversByHometown = function(drivers, location) {
+  newArray = drivers.filter(driver => driver.hometown === location);
+  newArray.map(driver => console.log(driver.name));
+}
+
+function compareDrivers(a,b) {
+  let result = 0;
+  if (a.revenue > b.revenue) {
+    result = 1;
+  } else if (b.revenue > a.revenue) {
+    result = -1;
+  }
+  return result;
+}
+
+const driversByRevenue = function(drivers) {
+  console.log(compareDrivers);
+}
